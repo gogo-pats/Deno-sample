6ae25dafe6858f532aeb6dbe47a132efe429824b
gogo-pats <project999111@gmail.com>
2021-09-29 17:54:39 +0900

    add code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Deno Servest
+
+### Deno実行(srcで実施)
+```
+deno run --allow-net app/main.ts
+```
+
---
+++ src/app/main.ts
@@ -0,0 +1,14 @@
+import {
+  createApp,
+} from "https://deno.land/x/servest@v1.3.1/mod.ts";
+const app = createApp();
+app.handle("/", async (req) => {
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/plain",
+    }),
+    body: "hello World!",
+  });
+});
+app.listen({ port: 3000 });
